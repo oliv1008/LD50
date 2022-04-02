015ab51b9e5ff12d002bebe8bd0301d455bbe00e
Aymeric Le Moal <aymeric.le.moal7@gmail.com>
2022-04-02 23:59:24 +0200

    on peut créer de l'eau + refactoring du dig

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool canCreateWater = false;
+    public bool canDig = false;
+
+    private Tilemap destructibleTilemap;
+    private Vector3 mousePos;
+
+    [SerializeField]
+    private GameObject waterParticule;
+
+    private void Start()
+    {
+        destructibleTilemap = GameObject.FindGameObjectWithTag("Destructible").GetComponent<Tilemap>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+
+            if (canDig)
+            {
+                destructibleTilemap.SetTile(destructibleTilemap.WorldToCell(mousePos), null);
+            }
+
+            if(canCreateWater)
+            {
+                bool isEmptySpace = true;
+
+                if(destructibleTilemap.GetTile(destructibleTilemap.WorldToCell(mousePos)) == null)
+                        Instantiate(waterParticule, mousePos, Quaternion.identity);
+            }
+        }
+    }
+}
